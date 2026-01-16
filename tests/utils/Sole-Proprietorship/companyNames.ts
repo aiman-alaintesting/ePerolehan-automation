f8bb34227f396d7a9d0de83db8cf55477de0ab7d
aiman-alaintesting <aiman@alaintesting.com.my>
2026-01-16 15:51:54 +0800

    commit

--- tests/utils/Sole-Proprietorship/companyNames.ts
+++ tests/utils/Sole-Proprietorship/companyNames.ts
@@ -1,16 +1,15 @@
 // companyNames.ts
 export const companyNames: string[] = [
 
-  'ROB-Sole Consultant 011',
-  'ROB-Sole Consultant 012',
-  'ROB-Sole Consultant 013',
-  'ROB-Sole Consultant 014',
-  'ROB-Sole Consultant 015',
-  'ROB-Sole Consultant 016',
-  'ROB-Sole Consultant 017',
-  'ROB-Sole Consultant 018',
-  'ROB-Sole Consultant 019',
-  'ROB-Sole Consultant 020',
-
+  'ROB - Partnership 16',
+  'ROB - Partnership 17',
+  'ROB - Partnership 18',
+  'ROB - Partnership 19',
+  'ROB - Partnership 20',
+  'ROB - Partnership 21',
+  'ROB - Partnership 22',
+  'ROB - Partnership 23',
+  'ROB - Partnership 24',
+  'ROB - Partnership 25',
 
 ];
